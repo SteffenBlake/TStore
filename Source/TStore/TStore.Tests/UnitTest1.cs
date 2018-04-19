a1fd7bcd9b0fa76684e7eed354892a8075534691
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2018-04-18 18:23:20 -0600

    Project Bootstrap

---
+++ Source/TStore/TStore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TStore.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true);
+        }
+    }
+}
