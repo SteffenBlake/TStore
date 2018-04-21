44e8024c235208c21aa0246bcc05e99fab4cd720
Steffen Cole Blake <steffen.cole.blake@gmail.com>
2018-04-21 02:47:20 -0600

    [Part 2] Step 1: Simple Extensions

---
+++ Source/TStore/TStore/Extensions/ITStoreExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TStore.Interfaces;
+
+namespace TStore.Extensions
+{
+    public static class ITStoreExtensions
+    {
+        public static void Register(this ITStore store, Type type) => store.Register(type, type);
+        public static void Register<TKey, TValue>(this ITStore store) => store.Register(typeof(TKey), typeof(TValue));
+        public static void Register<T>(this ITStore store) => store.Register<T, T>();
+
+        public static T Fetch<T>(this ITStore store) => (T)store.Fetch(typeof(T));
+    }
+}
